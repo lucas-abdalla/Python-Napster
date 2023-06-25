b8e29117242bef0b075d2f63b356e0b06a24a3c0
lucas-abdalla <99680783+lucas-abdalla@users.noreply.github.com>
2023-06-25 00:05:03 -0300

    Added comments to the code adn changed class names. Changed chunks size to 0,5MB

--- client.py
+++ client.py
@@ -3,110 +3,162 @@ import os
 import pickle
 import threading
 
-class peer:
+class Peer:
 
+    #Cria um socket para a classe Peer
     s = socket.socket()
 
+    #Inicializa o peer com as informações capturadas do teclado e obtem sua lista de arquivos
     def __init__(self, IP, port, path):
         self.IP = IP
         self.port = port
         self.path = path
+        #Obtém todos os arquivos no diretório em path
         self.files = os.listdir(path)
 
+    #Faz requisição de atualização ao servidor e atualiza sua própria lista de arquivos
+    #Função UPDATE do peer
     def update(self):
-        #conecta com o servidor e atualiza arquivos possuídos
+        self.files.append(query)
+        #Diz ao servidor que é uma requisição de update
         self.s.sendall("UPDATE".encode("utf-8"))
+        #Envia ao servidor arquivo adicionado
         self.s.sendall(query.encode("utf-8"))
+        #Agurada o UPDATE_OK
         resposta = self.s.recv(4096).decode("utf-8")
         while True:
+            #Se UPDATE_OK, libera o peer
             if resposta == "UPDATE_OK":
                 break
 
+    #Função JOIN do peer
     def join(self):
+        #Conecta ao servidor
         self.s.connect((self.IP, self.port))
+        #Usa módulo pickle do Python para codificar o vetor de arquivos do peer em bytes
         data_string = pickle.dumps(self.files)
+        #Envia o vetor de arquivos em bytes
         self.s.sendall(data_string)
+        #Recebe o JOIN_OK
         resposta = self.s.recv(4096).decode()
         if resposta == "JOIN_OK":
+            #Printa conforme especificação
             print(f'Sou peer {self.s.getsockname()[0]}:{self.s.getsockname()[1]} com arquivos', end = " ")
             for file in self.files:
                 print(file, end = " ")
         print()
+        #Após dar JOIN no servidor, cria uma thread que escutará requisições de download vindas de outros peers
         standby_thread = threading.Thread(target=self.standbyD, args=())
         standby_thread.start()
 
+    #Função SEARCH do peer
     def search(self):
+        #Global para poder ser utilizada no download e update futuramente
         global query
         query = input()
+        #Envia query ao servidor
         self.s.sendall(query.encode("utf-8"))
+        #Recebe resposta do SEARCH já formatada conforme especificação pelo servidor
         resposta = self.s.recv(4096).decode()
         print(resposta)
         print()
 
+    #Função DOWNLOAD do peer
     def download(self):
+        #Recebe IP e porta do peer ao qual fará a requisição
         IP = input()
         port = int(input())
+        #Abre um novo socket para fazer a requisição de download
         d = socket.socket()
         d.connect((IP, port))
+        #Se foi feita uma pesquisa por arquivo ao servidor prossegue com a requisição
         if query:
+            #Envia o nome do arquivo requisitado ao outro peer
             d.sendall(query.encode("utf-8"))
+            #Recebe o tamanho do arquivo requisitado
             file_size = int(d.recv(4096).decode("utf-8"))
+            #Abre arquivo no modo escrita na pasta path do peer
             with open(self.path + "\\" + query, "wb") as f:
                 i = 0
+                #Recebe os dados em pedaços de 0,5MB até que atinge o tamanho do arquivo
                 while i < file_size:
-                    data = d.recv(1024 * 1024)
-                    i += 1024 * 1024
+                    data = d.recv(512 * 1024)
+                    #Usado para controlar quantos bytes já foram recebidos
+                    i += 512 * 1024
                     f.write(data)
+                #Fecha o arquivo
                 f.close()
+            #Fecha a conexão com o peer requisitado
             d.close()
+            #Print conforme especificação
             print("Arquivo %s baixado com sucesso na pasta %s" % (query, self.path))
             print()
+            #Chama a função update
             self.update()
-        else:
-            print("Não foi possível baixar pois uma pesquisa por arquivo não foi feita")
     
+    #Função stand by download, que escuta requisições de downloads vindas de outros peers. Funciona numa thread separada criada no JOIN
     def standbyD(self):
+        #Cria e liga um socket ao host e porta desse peer
         sbD = socket.socket()
         sbD.bind((self.s.getsockname()[0], self.s.getsockname()[1]))
         sbD.listen(5)
+        #Espera requisições de downloads em looping infinito
         while True:
+            #Aceita conexão e recebe a requisição
             c, addr = sbD.accept()
             request = c.recv(4096).decode()
+            #Se possui o arquivo requisitado...
             if request in self.files:
+                #Cria um path com o nome do arquivo solicitado
                 file_path = os.path.join(self.path, request)
+                #Utiliza o path criado acima para obter tamanho do arquivo solicitado
                 file_size = os.path.getsize(file_path)
+                #Manda o tamanho do arquivo solicitado ao outro peer
                 c.sendall(str(file_size).encode("utf-8"))
+                #Abre arquivo no modo read
                 with open(file_path, "rb") as f:
                     i = 0
+                    #Similar ao download, envia o arquivo solicitado em pedações de 0,5MB
                     while i < file_size:
-                        c.sendall(f.read(1024 * 1024))
-                        i += 1024 * 1024
+                        c.sendall(f.read(512 * 1024))
+                        i += 512 * 1024
+                    #Fecha arquivo
                     f.close()
+                #Fecha conexão com o outro peer
                 c.close()
 
+#Função simples que printa o menu interativo no console
 def printMenu():
         print("Escolha uma das opções digitando os números indicados:\n")
         print("1: Conectar ao servidor (JOIN)")
         print("2: Pesquisar arquivo (SEARCH)")
         print("3: Baixar arquivo (DOWNLOAD)")
-    
+
+#Funcionamento lógico do menu interativo
 def menu(p):
     printMenu()
+    #Obtem ocção selecionada
     option = int(input())
     if option == 1:
+        #Obtém a inicialização do peer pelo teclado no console
         IP = input()
         port = int(input())
         path = input()
-        p = peer(IP, port, path)
+        #Inicializa peer
+        p = Peer(IP, port, path)
+        #Chama função JOIN
         p.join()
         menu(p)
+    #Chama função SEARCH
     elif option == 2:
         p.search()
         menu(p)
+    #Chama função DOWNLOAD
     elif option == 3:
         p.download()
         menu(p)
 
+#Cria variável global p para ser instância de peer após o join e manter mesma instância ao chamar o menu repetidamente
 global p
 p = None
 menu(p)
--- server.py
+++ server.py
@@ -2,76 +2,121 @@ import socket
 import threading
 import pickle
 
-class server:
+class Servidor:
     
+    #Cria o socket do servidor
     s = socket.socket()
+    #Cria estrutura que armazenará peers e seus arquivos
     peerList = []
     
+    #Inicializa o servidor com IP e porta lidos do teclado
     def __init__(self, IP, port):
-        server.IP = IP
-        server.port = port
-        server.s.bind((IP, port))
-        server.start_server()
+        Servidor.IP = IP
+        Servidor.port = port
+        Servidor.s.bind((IP, port))
+        Servidor.start_server()
 
+    #Função JOIN do servidor
     def join(c, addr, data):
+        #Reconverte a lista de arquivos do peer recebida em bytes para um array de strings
         files = pickle.loads(data)
+        #Obtém IP e porta do peer conectado
         c_ip, c_port = c.getpeername()
+        #Prepara formatação de string com IP e porta do peer
         peerInfo = str(c_ip) + ";" + str(c_port) + ";"
+        #Printa conforme especificação
         print(f'Peer {c_ip}:{c_port} adicionado com arquivos', end = " ")
         for file in files:
             print(file, end = " ")
+            #Continua formatação da string adicionando os arquivos do peer. Cada valor é separado por ;
             peerInfo += file + ";"
-        server.peerList.append(peerInfo)
+        #Adiciona as informações do peer na lista de peers do servidor
+        Servidor.peerList.append(peerInfo)
         print()
+        #Envia o JOIN_OK ao peer
         c.send("JOIN_OK".encode("utf-8"))
-        server.handle_client(c, addr)
+        #Volta a escutar as requisições do servidor
+        Servidor.handle_client(c, addr)
 
+    #Função SEARCH do servidor
     def search(c, addr, data):
+        #Prepara formatação da resposta
         resposta = "peers com arquivo solicitado: "
+        #Recebe arquivo pesquisado
         query = data.decode("utf-8")
+        #Obtém IP e porta do peer solicitante
         c_ip, c_port = c.getpeername()
+        #Printa conforme especificação
         print("Peer %s:%s solicitou arquivo %s" % (str(c_ip), str(c_port), query))
-        for peer in server.peerList:
+        #Percorre a lista de peers do servidor
+        for peer in Servidor.peerList:
+            #Faz o split da string nos ;
             peerArr = peer.split(';')
             i = 2
+            #Percorre cada nome de arquivo do peer atual. Começa no 2 porque 0 e 1 guardam IP e porta do peer
             for i in range(len(peerArr)):
+                #Se arquivo do peer da lista é o arquivo solicitado pelo peer conectado, adicionado peer da lista à resposta
                 if (peerArr[i] == query):
                     resposta += str(peerArr[0]) + ":" + str(peerArr[1]) + " "
+        #Envia resposta ao peer solicitante
         c.sendall(resposta.encode("utf-8"))
-        server.handle_client(c, addr)
+        Servidor.handle_client(c, addr)
 
+    #Função UPDATE do servidor
     def update(c, addr):
+        #Recebe os bytes utf-8 do nome do arquivo baixado
         data = c.recv(4096)
+        #Decodifica os bytes e obtém a string do nome
         file = data.decode("utf-8")
+        #Obtém IP e porta do peer que requisitou a atualização
         c_ip, c_port = c.getpeername()
-        for i in range (len(server.peerList)):
-            peerArr = server.peerList[i].split(';')
+        #Percorre lista de peers do servidor
+        for i in range (len(Servidor.peerList)):
+            #Faz o split
+            peerArr = Servidor.peerList[i].split(';')
+            #Se IP e porta do peer atual é o mesmo do peer requisitante, adiciona o arquivo baixado ao seu registro
             if (str(c_ip) == peerArr[0] and str(c_port) == peerArr[1]):
-                server.peerList[i] += file + ";"
+                Servidor.peerList[i] += file + ";"
+        #Responde com UPDATE_OK
         c.sendall("UPDATE_OK".encode("utf-8"))
-        server.handle_client(c, addr)
+        Servidor.handle_client(c, addr)
 
+    #Função que cuida das requisições dos peers. É chamada por diferentes threads para cada peer conectado
     def handle_client(c, addr):
+        #Captura problemas de desconexão e outros erros que possam ocorrer
         try:
+            #Recebe dados do peer em bytes
             data = c.recv(2097152)
+            #Verifica se os dados são do tipo pickle verificando se a primeira posição é 0x80, que é o início da codificação pickle.
+            #Se os dados são do tipo pickle significa que é uma requisição do tipo join
             if data[0] == 0x80:
-                server.join(c, addr, data)
+                Servidor.join(c, addr, data)
             else:
+                #Se não é pickle, caso tenha recebido uma string UPDATE, é uma requisição UPDATE
                 aux = data.decode("utf-8")
                 if aux == "UPDATE":
-                    server.update(c, addr)
+                    Servidor.update(c, addr)
+                #Se não era uma requisição update era uma requisição SEARCH. Os bytes codificados são enviados à função SEARCH e são tratados nela
                 else:
-                    server.search(c, addr, data)
+                    Servidor.search(c, addr, data)
+        #Caso capture um erro não há prints para manter no console somente os prints especificados
         except Exception as e:
             pass
 
+    #Função que inicia o servidor
     def start_server():
-        server.s.listen(5)
+        #Começa a escutar por peers
+        Servidor.s.listen(5)
+        #Sempre escutando
         while True:
-            c, addr = server.s.accept()
-            c_thread = threading.Thread(target=server.handle_client, args=(c, addr))
+            #Aceita conexão
+            c, addr = Servidor.s.accept()
+            #Envia novas conexões para threads para poder cuidar de múltiplos peers ao mesmo tempo
+            c_thread = threading.Thread(target=Servidor.handle_client, args=(c, addr))
             c_thread.start()
 
+#Captura do teclado IP e porta do servidor
 IP = input()
 port = int(input())
-svr = server(IP, port)
+#Instancia o servidor
+svr = Servidor(IP, port)
